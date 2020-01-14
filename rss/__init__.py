da31ff6f704c8aa0abc70cc8f59e6770c760e004
Michael H <michael@michaelhall.tech>
2020-01-13 20:35:51 -0500

    meh..

--- rss/__init__.py
+++ rss/__init__.py
@@ -1,7 +1,14 @@
+import functools
+import sys
+import subprocess
 from redbot.core.errors import CogLoadError
 
 
 def setup(bot):
+    bot.loop.create_task(_pre_setup(bot))
+
+
+def _setup(bot):
     try:
         import feedparser
     except ImportError:
@@ -18,3 +25,18 @@ def setup(bot):
     from .core import RSS
 
     bot.add_cog(RSS(bot))
+
+
+async def _pre_setup(bot):
+    command = f'{sys.executable} -m pip install -U --pre "feedparser>=6.0.0b1,<6.1.0"'
+    await bot.loop.run_in_executor(
+        None,
+        functools.partial(
+            subprocess.run,
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=True,  # nosec
+        ),
+    )
+    _setup(bot)
